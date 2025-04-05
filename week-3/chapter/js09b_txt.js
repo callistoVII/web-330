8aae20ced1c082402a80fa3149668c05e1c28615
Nyx <xamarynth@gmail.com>
2025-04-05 14:25:13 -0400

    Week 3 - Chapter Work and Project 9-02

---
+++ week-3/chapter/js09b_txt.js
@@ -0,0 +1,11 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 9
+      Chapter case
+
+      Eating Well in Season
+      Author: Nicole Nielsen
+      Date:   04/04/2025
+
+      Filename: js09b.js
+ */
---
+++ week-3/chapter/js09c_txt.js
@@ -0,0 +1,12 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 9
+      Chapter case
+
+      Eating Well in Season Retrieving Data from Local Storage
+      Author: Nicole Nielsen
+      Date:   04/04/2025
+
+      Filename: js09c.js
+ */
+
